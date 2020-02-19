4ac67d4137f38a3db7ab617243ee3ccc600a6a1d
Vitaly-V <gtopbox@gmail.com>
2020-02-19 12:25:01 +0200

    Add app bar

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(BuildContext context) {
+  return AppBar(
+    title: Text(
+      'FlutterShare',
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: 'Signatra',
+        fontSize: 50,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
