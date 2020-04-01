cdf36dbe2cfa5c05d126be56a7c364ff9c77b1ef
Vitaly-V <gtopbox@gmail.com>
2020-04-01 17:11:26 +0300

    Add firebase functions

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.onCreateFollower = functions.firestore
+  .document('/followers/{userId}/userFollowers/{followerId}')
+  .onCreate(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+    const followedUserRef = admin
+      .firestore()
+      .collection('posts')
+      .doc(userId)
+      .collection('userPosts');
+
+    const timelinePostRef = admin
+      .firestore()
+      .collection('timeline')
+      .doc(userId)
+      .collection('timelinePosts');
+
+    const querySnapshot = await followedUserRef.get();
+    querySnapshot.forEach(doc => {
+      if(doc.exists) {
+        const postId = doc.id;
+        const postData = doc.data();
+        timelinePostRef.doc(postId).set(postData);
+      }
+    });
+  });
