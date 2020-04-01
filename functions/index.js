b310d97fda0c0ac36a0e483c1eb85120551142a3
Vitaly-V <gtopbox@gmail.com>
2020-04-01 17:51:14 +0300

    Add unfollowing

--- functions/index.js
+++ functions/index.js
@@ -21,10 +21,31 @@ exports.onCreateFollower = functions.firestore
 
     const querySnapshot = await followedUserRef.get();
     querySnapshot.forEach(doc => {
-      if(doc.exists) {
+      if (doc.exists) {
         const postId = doc.id;
         const postData = doc.data();
         timelinePostRef.doc(postId).set(postData);
+      }
+    });
+  });
+
+exports.onDeleteFollower = functions.firestore
+  .document('/followers/{userId}/userFollowers/{followerId}')
+  .onDelete(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+
+    const timelinePostRef = admin
+      .firestore()
+      .collection('timeline')
+      .doc(userId)
+      .collection('timelinePosts')
+      .where('ownerId', '==', userId);
+
+    const querySnapshot = await timelinePostRef.get();
+    querySnapshot.forEach(doc => {
+      if (doc.exists) {
+        doc.ref.delete();
       }
     });
   });
